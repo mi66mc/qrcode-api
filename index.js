ee7d2f78e218d6872e8bcb0b17c7b83e27f63d73
mi66mc <miguelmalgarezi@gmail.com>
2023-01-24 09:23:37 -0300

    Added library 'qr-image-color' to improve colors in the QR Codes

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express")
-const qr = require("qr-image")
+const qr = require("qr-image-color")
 const app = express()
 
 app.get("/qr", (req, res) => {
-    const qrcontent = req.query.content || "https://github.com/mi66mc"
-    const qrimage = qr.image(qrcontent, { type: "png" })
+    try {
+        const qrcontent = req.query.content || "https://github.com/mi66mc"
+        const qrcolor = req.query.color || "black"
+        const qrbackground = req.query.background
+        const qrimage = qr.image(qrcontent, { type: "png", color:qrcolor, background:qrbackground })
 
-    qrimage.pipe(res)
+        qrimage.pipe(res)
+    } catch (e) {
+        res.json({error:"An error has happened."})
+    }
 })
 
 app.listen(3000)
